d6e1467f9af5c0c07997ee40bd072ffee6fc790b
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2013-11-16 00:23:17 -0500

    symlink.sh functional

--- hide-bin/symlink.sh
+++ hide-bin/symlink.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for i in `find ~/dotfiles -type f -print | egrep -v '\.git|hide-bin|README' | sed s-$HOME/dotfiles/--` ; do
+for i in `find ~/dotfiles -type f -print | egrep -v '\.git/|hide-bin|README' | sed s-$HOME/dotfiles/--` ; do
     SOURCE=$HOME/dotfiles/$i
     TARGET=$HOME/$i
     DPATH=`echo $TARGET | sed s'-\(^.*/\).*$-\1-'`
-    echo rm $TARGET ';' mkdir -p $DPATH ';' ln -s $HOME/dotfiles/$i $HOME/$i
+
+    if [ -e "$TARGET" ] ; then
+	rm $TARGET 
+    fi
+
+    if [ ! -d "$DPATH" ] ; then
+	mkdir -p $DPATH 
+    fi
+
+    ln -s $HOME/dotfiles/$i $HOME/$i
 done
