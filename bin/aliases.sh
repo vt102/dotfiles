799d91f7b9ce2f9128d8a7465764937764efa8f2
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2013-11-17 16:01:31 -0500

    Shuffled aliases a bit.

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in `find ~/dotfiles/aliases -name '*[^~]' -type f -print` ; do
+# this will set up aliases on initial login, where our dotfiles .bashrc
+# didn't exist yet.  This file must be SOURCED, not run as script.
+
+for i in `find ~/dotfiles/tsl/.aliases -name '*[^~]' -type f -print` ; do
     # echo 'to open:' $i
     source $i
 done
